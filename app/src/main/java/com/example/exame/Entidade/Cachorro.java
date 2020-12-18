ce6541f84e90a9cd28ec858d3f7b77bfb7161814
unknown <dferrantisc@hotmail.com>
2020-12-17 20:02:04 -0400

    Com o mesmo projeto criado na atividade anterior. 1 - adicione um recicleView na activity main. 2 - Crie uma entidade cachorro. 3 - implemente um adapter para seu recicleView para listar a entidade. Cole o código do commit na resposta

---
+++ app/src/main/java/com/example/exame/Entidade/Cachorro.java
@@ -0,0 +1,15 @@
+package com.example.exame.Entidade;
+
+public class Cachorro {
+
+    private String idDog;
+    private String nomeDog;
+
+    public String getIdDog() {  return idDog;    }
+
+    public void setIdDog(String idDog) {  this.idDog = idDog;  }
+
+    public String getNomeDog() {    return nomeDog;    }
+
+    public void setNomeDog(String nomeDog) {     this.nomeDog = nomeDog;   }
+}
--- app/src/main/java/com/example/exame/MainActivity.java
+++ app/src/main/java/com/example/exame/MainActivity.java
@@ -3,23 +3,39 @@ package com.example.exame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
+import com.example.exame.Entidade.Cachorro;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseDatabase database;
-    DatabaseReference DBRefe;
+    private List<Cachorro> dogList = new ArrayList<>();
+    private ArrayAdapter<Cachorro> dogArrayAdapter;
+
+    EditText Nome;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Nome = (EditText)findViewById(R.id.txtNome);
+        list = (ListView) findViewById(R.id.listView);
 
-        FirebaseApp.initializeApp(MainActivity.this);
-        //database = FirebaseDatabase
+
+
+
+
+
+
+
+
     }
 }
